076953fa8127bfacf46fd5f227e58408ee2aebb9
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-18 14:25:17 -0300

    initial commit

---
+++ InstagramClone_SwiftUI/InstagramClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramClone_SwiftUIApp.swift
+//  InstagramClone_SwiftUI
+//
+//  Created by Lucas Newlands on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
